0c089b5a68e8d9049deff88e62068331b190dfba
adarsh-wagh <adarshwagh21@gmail.com>
2026-02-21 17:40:35 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div>
+    <h1 className="font-game text-2xl">yefwgf4ewrgdp</h1>
+    <Button>hola</Button>
+  </div>
+  );
+}
